7c023f73669b536516927952ece9def4349396b9
Tortue-Enjoye <tom.pottin@gmail.com>
2026-04-02 16:11:40 +0200

    Modification pour implémentation joueur

--- game.py
+++ game.py
@@ -1,10 +1,21 @@
 import pygame
+from bird import Bird
 
 class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((800, 600))
         self.running = True
+        self.clock = pygame.time.Clock()
+
+        #Gestion joueur
+        self.bird_co = (0, 0)
+        self.bird = Bird(self.bird_co)
+
+
+        #Gestion jeu
+        self.gravity = 0.5
+
 
     def run(self):
         while self.running:
@@ -12,10 +23,19 @@ class Game:
                 if event.type == pygame.QUIT:
                     self.running = False
 
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.bird.move()
+
             self.update()
+            self.clock.tick(60)
 
         pygame.quit()
 
     def update(self):
-        self.screen.fill((100,100,100))
+        self.screen.fill((100, 100, 100))
+
+        self.bird.velocity += self.gravity
+        self.bird.update()
+
+        self.screen.blit(self.bird.image, (self.bird.x, self.bird.y))
         pygame.display.flip()
