afa0b8227f773556ab4ce793b688218a5b8c38ba
Tortue-Enjoye <tom.pottin@gmail.com>
2026-04-03 13:43:03 +0200

    Ajout de 'hover' dans le menu
    Ajout d'un hover sur les boutons 'Jouer' et 'Quitter' dans le menu

--- menu.py
+++ menu.py
@@ -6,7 +6,11 @@ class Menu:
         self.screen = screen
         self.clock = clock
         self.font = pygame.font.Font("assets/fonts/JetBrainsMono-Bold.ttf", 72)
-        self.font_small = pygame.font.Font("assets/fonts/JetBrainsMono-Bold.ttf", 36)
+        self.font_small = pygame.font.Font("assets/fonts/JetBrainsMono-Bold.ttf", 48)
+        self.font_hover = pygame.font.Font("assets/fonts/JetBrainsMono-Bold.ttf", 64)
+
+        self.play_rect = pygame.Rect(0, 0, 0, 0)
+        self.quit_rect = pygame.Rect(0, 0, 0, 0)
 
     def run(self):
         while True:
@@ -25,17 +29,27 @@ class Menu:
     def draw(self):
         self.screen.fill((144, 213, 255))
 
+        mouse_pos = pygame.mouse.get_pos()
+
         # Titre
-        title = self.font.render("Flappy Bird", True, (255, 255, 255))
+        title = self.font.render("Flappy Bird", True, (0, 76, 153))
         self.screen.blit(title, (400 - title.get_width() // 2, 150))
 
-        # Bouton Joueur
-        play_text = self.font_small.render("Jouer", True, (255, 255, 255))
+        # Bouton Jouer
+        if self.play_rect.collidepoint(mouse_pos):
+            play_text = self.font_hover.render("Jouer", True, (0, 76, 153))
+        else:
+            play_text = self.font_small.render("Jouer", True, (0, 0, 0))
+
         self.play_rect = play_text.get_rect(center=(400, 320))
         self.screen.blit(play_text, self.play_rect)
 
         # Bouton Quitter
-        quit_text = self.font_small.render("Quitter", True, (255, 255, 255))
+        if self.quit_rect.collidepoint(mouse_pos):
+            quit_text = self.font_hover.render("Quitter", True, (0, 76, 153))
+        else:
+            quit_text = self.font_small.render("Quitter", True, (0, 0, 0))
+
         self.quit_rect = quit_text.get_rect(center=(400, 390))
         self.screen.blit(quit_text, self.quit_rect)
 
